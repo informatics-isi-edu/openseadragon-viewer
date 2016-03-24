eaa8ed1c5c0bc6ad9d60e8d23382682a4379c5f0
Mei-Hui Su <mei@Mei-Huis-MacBook-Air.local>
2016-03-24 01:01:32 -0700

    1) change the scalebar's color

--- js/osd.viewer_multi.js
+++ js/osd.viewer_multi.js
@@ -120,8 +120,8 @@ jQuery(document).ready(function() {
             yOffset: 10,
             stayInsideImage: true,
             color: "rgb(153, 0, 0)",
-            fontColor: "rgb(153, 0, 0)",
-            backgroundColor: "rgba(255, 255, 255, 0.0)",
+            fontColor: "rgb(255, 255, 255)",
+            backgroundColor: "rgba(238, 238, 238, 0.2)",
             fontSize: "small",
             barThickness: 2
     });
