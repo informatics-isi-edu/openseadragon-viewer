0d32f3e97946ec3eacd36a05a3b7955e6f454a92
Mei-Hui Su <mei@Mei-Huis-MacBook-Air.local>
2016-12-10 01:27:56 -0800

    alter the color of channel sliders change the expand icon and also the show/hide check box

--- js/osd.filtering_ui.js
+++ js/osd.filtering_ui.js
@@ -330,7 +330,8 @@ function addItemListEntry(n,i,dir,hue,contrast,brightness,opacity,gamma,alias=nu
   var _nn='';
 //hue hint from http://hslpicker.com/#00e1ff
 
-_nn+=' <div class="panel panel-default col-md-12"> <input type="checkbox" id="'+_visible_name+'" class="pull-left" checked="" style="margin:10px"  onClick="toggleItem('+i+',\''+name+'\')" title="uncheck to hide the channel"/>';
+_nn+=' <div class="panel panel-default col-md-12">';
+_nn+='<button id="'+_visible_name+'" class="pull-left" style="margin-top:10px; margin-right:5px; border:none;background-color:white"  onClick="toggleItem('+i+',\'eye_'+name+'\')" title="hide or show channel"><span id="eye_'+name+'" class="glyphicon glyphicon-eye-open" style="color:#337ab7;"></span> </button>';
 
  var aname=name;
  if(alias != null)
@@ -339,16 +340,16 @@ _nn+=' <div class="panel-heading"> <h5 class="row panel-title" style="background
 
 _nn+=' <div id="'+_collapse_name+'" class="panel-collapse collapse"> <div class="panel-body">';
 
-_nn+= ' <div id="'+name+ '" title="restore settings" class="row" style="background-color:white;opacity:1;"> <button id="'+_reset_btn+ '" type="button" class="btn btn-xs btn-primary pull-right" onclick="toggleResetItem('+ i+ ','+ '\''+ name+ '\');" style="font-size:14px;margin-top:2px; margin-right:20px" >Reset</button><div class="filtercontrol">';
+_nn+= ' <div id="'+name+ '" title="restore settings" class="row" style="background-color:white;opacity:1;"> <button id="'+_reset_btn+ '" type="button" class="btn btn-xs btn-primary pull-right" onclick="toggleResetItem('+ i+ ','+ '\''+ name+ '\');" style="font-size:12px;margin-top:2px; margin-right:20px" >Reset</button><div class="filtercontrol">';
 
 //contrast slider...
-_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Contrast<input id="'+ _contrast_btn+'" type="button" class="btn btn-info pull-right"  value="0" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"></div> <div id="'+ _contrast_name+'" class="slider" style="background:yellow;"> </div></div>';
+_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Contrast<input id="'+ _contrast_btn+'" type="button" class="btn btn-info pull-right"  value="0" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"></div> <div id="'+ _contrast_name+'" class="slider" style="background:#337ab7;"> </div></div>';
 
 //brightness slider...
-_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Brightness<input id="'+ _brightness_btn+'" type="button" class="btn btn-info pull-right"  value="0" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"> </div> <div id="'+_brightness_name+'" class="slider" style="background:green;"> </div> </div>';
+_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Brightness<input id="'+ _brightness_btn+'" type="button" class="btn btn-info pull-right"  value="0" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"> </div> <div id="'+_brightness_name+'" class="slider" style="background:#337ab7;"> </div> </div>';
 
 //gamma slider...
-_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Gamma<input id="'+ _gamma_btn+'" type="button" class="btn btn-info pull-right"  value="1" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"> </div> <div id="'+_gamma_name+'" class="slider" style="background:blue;"> </div> </div>';
+_nn+= '<div class="col-md-12 filter-slider"> <div class="menuLabel">Gamma<input id="'+ _gamma_btn+'" type="button" class="btn btn-info pull-right"  value="1" style="color:black; background:white; height:16px; width:30px; font-size:12px; padding:0px;"> </div> <div id="'+_gamma_name+'" class="slider" style="background:#337ab7;"> </div> </div>';
 
 // opacity slider... disable by default 
 _nn+= '<div class="col-md-12 filter-slider" style="display:none"> <div class="menuLabel">Opacity<input id="'+_opacity_btn+'" type="button" class="btn btn-info pull-right" value="1" style="color:black; background:white; height:16px; width:30; margin-left:10px; font-size:12px; padding:0px;"> </div> <div id="'+_opacity_name+'" class="slider" style="background:grey;"> </div> </div>';
@@ -574,12 +575,16 @@ window.console.log("bad panic..",propertyList.length," - ", initPropertyList.len
 /* 0 , 1 */
 function toggleItem(itemID, itemLabel) {
   var item=myViewer.world.getItemAt(itemID);
+  var tmp='#'+itemLabel;
+  var eptr = $(tmp);
   var op=item.getOpacity();
   if (op > 0) {
     item.setOpacity(0);
+    eptr.removeClass('glyphicon-eye-open').addClass('glyphicon-eye-close');
     } else {
       op=_getOpacityByID(itemID);
       item.setOpacity(op);
+      eptr.removeClass('glyphicon-eye-close').addClass('glyphicon-eye-open');
   }
 }
 
