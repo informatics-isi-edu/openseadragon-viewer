68bccbdd9144403168654292b1b7d5dd8499e2fe
Bhavya Rastogi <rastogibhavya94@gmail.com>
2020-08-20 12:44:28 -0700

    Removed redundant attributes

--- js/viewer/annotation/polygon.js
+++ js/viewer/annotation/polygon.js
@@ -5,10 +5,6 @@ function Polygon(attrs){
 
     this._tag = "polygon";
     this._attrs["points"] = attrs["points"] || "";
-    this._attrs["fill"] = attrs["fill"] || "";
-    this._attrs["stroke"] = attrs["stroke"] || "";
-    this._attrs["stroke-width"] = attrs["stroke-width"] || "";
-    this._attrs["stroke-linecap"] = attrs["stroke-linecap"] || "";
     this.svg = null;
     this.renderLine = null;
     this.dataPoints = [];
